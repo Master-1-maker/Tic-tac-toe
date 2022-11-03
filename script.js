67d61d58cff0b52d7a202d14ba35fbb0df9af9d6
Master-1-maker <97612721+Master-1-maker@users.noreply.github.com>
2022-11-03 14:09:32 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,57 @@
-//player factory function
-const createPlayer = (name,marker) => {
-    return {name,marker}
-}
+
+const checkPlayerMeetsConditons = (boardNodes,targetPlayer) => {
+    const currResult = []
+
+    //winning conditons
+    const winningConditons = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,8],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6],
+    ];
+
+    boardNodes.forEach(({
+        innerHTML: val
+      }, i) => {
+        if (val === targetPlayer) {
+          currResult.push(i);
+        }
+      });
+
+      let isWinner = false
+
+      winningConditons.forEach((condition) => {
+        const isMatched = condition.reduce((isMatched, currVal, i) => currResult[i] !== currVal ? false : isMatched, true)
+
+        if (isMatched) {
+            isWinner = true;
+            return;
+        }
+      });
+      return isWinner;
+   }
+
+   //check for winner
+const checkWinner = () => {
+
+    const fields = document.querySelectorAll(".field");
+    
+    const isOWinner = checkPlayerMeetsConditons(fields, "O");
+    const isXWinner = checkPlayerMeetsConditons(fields, "X");
+  
+    if (isOWinner && isXWinner) { // when tie
+      document.querySelector(".message").innerHTML = "Game Tie";
+    } else if (isOWinner) {
+      document.querySelector(".message").innerHTML = "O is Winner";
+    } else if (isXWinner) {
+      document.querySelector(".message").innerHTML = "X is Winner";
+    }
+  
+  }
 
 //gameboard object
 const gameBoard = (() => {
@@ -11,40 +61,47 @@ const gameBoard = (() => {
         board.push("")
     }
 
-    //add event listeners on each field that will mark field
+
+    //add event listeners on each field that will mark field and change player's turn state
     const fields = document.querySelectorAll(".field") 
+    const nextTurn = document.querySelector(".message")
+
+   
     let turn = 0
+    
     fields.forEach((field) => {
-        field.addEventListener('click', () => {
-            if(turn %= 2) {
-                field.innerHTML = "O"}
-                else {
-                field.innerHTML = "X"}
-                turn ++
-            
-        })
+        field.addEventListener('click',handleClick,{once:true})
+
+        function handleClick() {
+
+            if (turn %= 2) {
+              field.innerHTML = "O"
+              field.classList.toggle("marker")
+              nextTurn.innerHTML = "Player X's turn"
+            } else {
+              field.innerHTML = "X"
+              field.classList.toggle("marker")
+              nextTurn.innerHTML = "Player O's turn"
+            }
+      
+            turn++
+      
+            checkWinner(); //check the winner
+
+        }
+      
     })
 
+    //restart game
+    const restart = document.getElementById("restart-button")
+
+    restart.addEventListener("click",clickE)
+
+    function clickE(){
+      return location.reload(true)
+    }
+
     return {
-        board
+      board, 
     }
 })()
-
-// game object
-const game = (() => {
-
-    //declare players
-    const playerOne = createPlayer("player x", "x")
-    const playerTwo = createPlayer("player o", "o")
-
-    //starting point
-    let activePlayer = playerOne
-    let winnerDeclared = false
-    let remainingSpots = 9
-
-    //selectors
-    let subtext
-
-    
-}
-)()
